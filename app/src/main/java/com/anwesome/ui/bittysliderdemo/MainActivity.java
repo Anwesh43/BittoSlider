a17c85abe0ead01b3f39b4ac9df40d5b3439954a
Anwesh43 <anweshthecool0@gmail.com>
2017-01-05 03:33:26 +0530

    onChangeListener for slider

--- app/src/main/java/com/anwesome/ui/bittysliderdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/bittysliderdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.anwesome.ui.bittysliderdemo;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.anwesome.ui.bittoslider.BittoSliderView;
+import com.anwesome.ui.bittoslider.SliderOnChangeListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
+        final TextView textView = (TextView)findViewById(R.id.slider_value);
+        BittoSliderView sliderView = (BittoSliderView)findViewById(R.id.slider);
+        sliderView.setOnChangeListener(new SliderOnChangeListener() {
+            @Override
+            public void onChange(float value) {
+                textView.setText(""+(int)Math.floor(value));
+            }
+        });
     }
 
 }
